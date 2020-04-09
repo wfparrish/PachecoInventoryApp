e82e63d8124c9366b9b0e86c77ffa96452590f2d
wfparrish <williparrish@gmail.com>
2020-04-08 19:11:36 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const handlebars = require('express-handlebars').create({ defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('home')
+});
+
+//Boilerplate for 404, 500, and listening on port 3000
+
+app.use((req, res, next) => {
+  res.status(404);
+  res.render('404');
+})
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500);
+  res.send('500');
+});
+
+app.listen(app.get('port'), () => {
+  console.log( 'Express started on http://localhost:' +
+    app.get('port') + '; press Ctrl-C to terminate.' );
+})
