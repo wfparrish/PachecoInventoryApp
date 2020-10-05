e184014933a8a1c7c75be2cade91d66483e35210
root <root@DESKTOP-2JGG68N.localdomain>
2020-10-05 10:24:58 -0600

    updated packages

--- routes/api/sectors.js
+++ routes/api/sectors.js
@@ -1,52 +1,50 @@
 const express = require('express');
-const SectorModel = require('../../models/Sector')
+const SectorModel = require('../../models/Sector');
 const router = express.Router();
 
 // @route GET api/sectors
 // @desc Test sectors retrieval
 // @access Public
-  router.get('/api/sectors', async (req, res) => {
-    const sectors = await SectorModel.find({});
-    //res.send('Sectors route');
-    
-      try {
-        res.send(sectors);
-      } catch (err) {
-        res.status(500).send(err);
-      }
-    });
-    
-    router.post('/api/sector', async (req, res) => {
-      const sector = new SectorModel(req.body);
-      try {
-        await sector.save();
-        res.send(sector);
-      } catch {
-        res.status(500).send(err);
-      }
-    });
-    
-    router.delete('/api/sector/:id', async (req, res) => {
-      try {
-        const sector = await SectorModel.findByIdAndDelete(req.params.id);
-    
-        if (!sector) res.status(404).send("No item found");
-        res.status(200).send()
-    
-        } catch (err) {
-          res.status(500).send(err)
-        }
-    });
-    
-    router.patch('/api/sector/:id', async (req, res) => {
-      try {
-        await SectorModel.findByIdAndUpdate(req.params.id, req.body);
-        await SectorModel.save();
-        res.send(sector);
-      } catch (err) {
-        res.status(500).send(err);
-      }
-    })
-    
-module.exports = router;
+router.get('/api/sectors', async (req, res) => {
+  const sectors = await SectorModel.find({});
+  //res.send('Sectors route');
 
+  try {
+    res.send(sectors);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+router.post('/api/sector', async (req, res) => {
+  const sector = new SectorModel(req.body);
+  try {
+    await sector.save();
+    res.send(sector);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+router.delete('/api/sector/:id', async (req, res) => {
+  try {
+    const sector = await SectorModel.findByIdAndDelete(req.params.id);
+
+    if (!sector) res.status(404).send('No item found');
+    res.status(200).send();
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+router.patch('/api/sector/:id', async (req, res) => {
+  try {
+    await SectorModel.findByIdAndUpdate(req.params.id, req.body);
+    await SectorModel.save();
+    res.send(sector);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+module.exports = router;
--- routes/api/yards.js
+++ routes/api/yards.js
@@ -1,52 +1,50 @@
 const express = require('express');
-const YardModel = require('../../models/Yard')
+const YardModel = require('../../models/Yard');
 const router = express.Router();
 
 // @route GET api/sectors
 // @desc Test yard retrieval
 // @access Public
-  router.get('/api/yards', async (req, res) => {
-    const yards = await YardModel.find({});
-    //res.send('Yard route');
-    
-      try {
-        res.send(yards);
-      } catch (err) {
-        res.status(500).send(err);
-      }
-    });
-    
-    router.post('/api/yard', async (req, res) => {
-      const yard = new YardModel(req.body);
-      try {
-        await yard.save();
-        res.send(yard);
-      } catch {
-        res.status(500).send(err);
-      }
-    });
-    
-    router.delete('/api/yard/:id', async (req, res) => {
-      try {
-        const yard = await YardModel.findByIdAndDelete(req.params.id);
-    
-        if (!yard) res.status(404).send("No item found");
-        res.status(200).send()
-    
-        } catch (err) {
-          res.status(500).send(err)
-        }
-    });
-    
-    router.patch('/api/yard/:id', async (req, res) => {
-      try {
-        await YardModel.findByIdAndUpdate(req.params.id, req.body);
-        await YardModel.save();
-        res.send(yard);
-      } catch (err) {
-        res.status(500).send(err);
-      }
-    })
-    
-module.exports = router;
+router.get('/api/yards', async (req, res) => {
+  const yards = await YardModel.find({});
+  //res.send('Yard route');
 
+  try {
+    res.send(yards);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+router.post('/api/yard', async (req, res) => {
+  const yard = new YardModel(req.body);
+  try {
+    await yard.save();
+    res.send(yard);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+router.delete('/api/yard/:id', async (req, res) => {
+  try {
+    const yard = await YardModel.findByIdAndDelete(req.params.id);
+
+    if (!yard) res.status(404).send('No item found');
+    res.status(200).send();
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+router.patch('/api/yard/:id', async (req, res) => {
+  try {
+    await YardModel.findByIdAndUpdate(req.params.id, req.body);
+    await YardModel.save();
+    res.send(yard);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+module.exports = router;
